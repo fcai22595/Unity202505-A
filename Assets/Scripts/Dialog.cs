4b74ed8ac4790b1c95aff7ede73a386e1e768ab1
fcai22595 <fcai22595@gmail.com>
2025-05-14 12:00:41 +0800

    0514

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    [SerializeField]
+    private TMPWriter tmpWriter;
+    
+    [ContextMenu(itemName)]
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
